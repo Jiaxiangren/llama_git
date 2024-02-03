5c2cdf2db302e8e737edc1bf74036bf1bc0538f9
jzr0065 <jzr0065@auburn.edu>
2024-02-02 20:23:13 -0600

    chnage bash

--- auto.sh
+++ auto.sh
@@ -1,1 +1,5 @@
-CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 python Fed_llama.py --lr 1e-4 --path ./flearn/configs/LORA/webnlg.json
+# CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 python Fed_llama.py --lr 1e-4 --path ./flearn/configs/LORA/webnlg.json
+
+
+
+CUDA_LAUNCH_BLOCKING=1 CUDA_VISIBLE_DEVICES=0 python Fed_roberta_lora.py --lr 1e-4 --path ./flearn/configs/ours/rte.json
