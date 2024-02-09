6575f7304e602344dfcbc21b584e717e58f13caf
jzr0065 <jzr0065@auburn.edu>
2024-02-09 11:23:51 -0600

    add

--- auto_sum.sh
+++ auto_sum.sh
@@ -3,31 +3,30 @@
 
 
 #lora
-for dataset in qnli sst-2 cola mrpc rte boolq mpqa subj trec mr
+for dataset in rte
 do
-    python Fed_lora.py --lr 1e-4 --path ./flearn/configs/LORA/${dataset}.json | tee ./res/lora/${dataset}.txt
+    python Fed_lora.py --lr 5e-5 --path ./flearn/configs/LORA/${dataset}.json | tee ./res/lora/${dataset}.txt
 done
 
 # lpt
-for dataset in qnli sst-2 cola mrpc rte boolq mpqa subj trec mr
+for dataset in rte
 do
-    python Fed_lpt.py --lr 1e-4 --path ./flearn/configs/LPT/${dataset}.json | tee ./res/lpt/${dataset}.txt
+    python Fed_lpt.py --lr 5e-5 --path ./flearn/configs/LPT/${dataset}.json | tee ./res/lpt/${dataset}.txt
 done
 
 # attempt
-for dataset in qnli sst-2 cola mrpc rte boolq mpqa subj trec mr
-do
-    python Fed_lpt.py --lr 1e-4 --path ./flearn/configs/ATTEMPT/${dataset}.json | tee ./res/attempt/${dataset}.txt
+for dataset in rte
+    python Fed_lpt.py --lr 5e-5 --path ./flearn/configs/ATTEMPT/${dataset}.json | tee ./res/attempt/${dataset}.txt
 done
 
 #prompt
-for dataset in qnli sst-2 cola mrpc rte boolq mpqa subj trec mr
+for dataset in rte
 do
-    python Fed_lpt.py --lr 1e-4 --path ./flearn/configs/PROMPT/${dataset}.json | tee ./res/prompt/${dataset}.txt
+    python Fed_lpt.py --lr 5e-5 --path ./flearn/configs/PROMPT/${dataset}.json | tee ./res/prompt/${dataset}.txt
 done
 
 # v2
-for dataset in qnli sst-2 cola mrpc rte boolq mpqa subj trec mr
+for dataset in rte
 do
-    python Fed_v2.py --lr 1e-4 --path ./flearn/configs/V2/${dataset}.json | tee ./res/v2/${dataset}.txt
+    python Fed_v2.py --lr 5e-5 --path ./flearn/configs/V2/${dataset}.json | tee ./res/v2/${dataset}.txt
 done
