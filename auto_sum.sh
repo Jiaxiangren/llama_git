c486f2f881ed24af98a722c0070e523888945f53
jzr0065 <jzr0065@auburn.edu>
2024-02-09 12:53:20 -0600

    add

--- auto_sum.sh
+++ auto_sum.sh
@@ -9,24 +9,24 @@ do
 done
 
 # lpt
-for dataset in rte
-do
-    python Fed_lpt.py --lr 1e-4 --path ./flearn/configs/LPT/${dataset}.json | tee ./res/lpt/${dataset}.txt
-done
+# for dataset in rte
+# do
+#     python Fed_lpt.py --lr 1e-4 --path ./flearn/configs/LPT/${dataset}.json | tee ./res/lpt/${dataset}.txt
+# done
 
-# attempt
-for dataset in rte
-    python Fed_lpt.py --lr 1e-4 --path ./flearn/configs/ATTEMPT/${dataset}.json | tee ./res/attempt/${dataset}.txt
-done
+# # attempt
+# for dataset in rte
+#     python Fed_lpt.py --lr 1e-4 --path ./flearn/configs/ATTEMPT/${dataset}.json | tee ./res/attempt/${dataset}.txt
+# done
 
-#prompt
-for dataset in rte
-do
-    python Fed_lpt.py --lr 1e-4 --path ./flearn/configs/PROMPT/${dataset}.json | tee ./res/prompt/${dataset}.txt
-done
+# #prompt
+# for dataset in rte
+# do
+#     python Fed_lpt.py --lr 1e-4 --path ./flearn/configs/PROMPT/${dataset}.json | tee ./res/prompt/${dataset}.txt
+# done
 
-# v2
-for dataset in rte
-do
-    python Fed_v2.py --lr 1e-4 --path ./flearn/configs/V2/${dataset}.json | tee ./res/v2/${dataset}.txt
-done
+# # v2
+# for dataset in rte
+# do
+#     python Fed_v2.py --lr 1e-4 --path ./flearn/configs/V2/${dataset}.json | tee ./res/v2/${dataset}.txt
+# done
