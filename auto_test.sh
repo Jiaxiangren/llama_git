e408ffdf9a72226dbafef6f433673b3fba45973a
jzr0065 <jzr0065@auburn.edu>
2024-02-11 05:44:20 -0600

    add

--- auto_test.sh
+++ auto_test.sh
@@ -14,7 +14,10 @@
 # CUDA_VISIBLE_DEVICES=0 python Fed_llama_ours.py --lr 1e-4 --select_method increase --select_layer_num 25 --sort_type seqreo --path ./flearn/configs/ours/rte.json
 
 #SHORTFORMER
-CUDA_VISIBLE_DEVICES=0 python Fed_llama_ours.py --lr 7e-6 --select_method increase --select_layer_num 25 --sort_type shortformer --path ./flearn/configs/ours/rte.json
+# CUDA_VISIBLE_DEVICES=0 python Fed_llama_ours.py --lr 7e-6 --select_method increase --select_layer_num 25 --sort_type shortformer --path ./flearn/configs/ours/rte.json
 
 #vanila
-CUDA_VISIBLE_DEVICES=0 python Fed_llama_ours.py --lr 7e-6 --select_method increase --select_layer_num 32 --sort_type vanila --path ./flearn/configs/ours/rte.json
+# CUDA_VISIBLE_DEVICES=0 python Fed_llama_ours.py --lr 7e-6 --select_method increase --select_layer_num 32 --sort_type vanila --path ./flearn/configs/ours/rte.json
+
+#se
+CUDA_VISIBLE_DEVICES=0 python Fed_llama_ours.py --lr 7e-6 --select_method increase --select_layer_num 32 --sort_type se --path ./flearn/configs/ours/rte.json
