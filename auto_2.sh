737fde41847dea538e0cdfa395bf16c4b26d3f24
jzr0065 <jzr0065@auburn.edu>
2024-02-10 22:26:05 -0600

    add

--- auto_2.sh
+++ auto_2.sh
@@ -6,7 +6,7 @@ conda activate lora
 # attempt
 for dataset in qnli sst-2 cola mrpc rte boolq mpqa subj trec mr
 do
-    python Fed_lpt.py --lr 1e-4 --path ./flearn/configs/ATTEMPT/${dataset}.json | tee ./res/attempt/${dataset}.txt
+    CUDA_VISIBLE_DEVICES=0 python Fed_lpt.py --lr 1e-4 --path ./flearn/configs/ATTEMPT/${dataset}.json | tee ./res/attempt/${dataset}.txt
 done
 
 #prompt
--- auto_sum.sh
+++ auto_sum.sh
@@ -1,40 +1,18 @@
-# source ~/miniconda3/etc/profile.d/conda.sh
-# conda activate lora
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate lora
 
 
 #lora
-for lr in 7e-6
+for dataset in rte cola mrpc mpqa subj trec mr
 do
-    for dataset in rte
-    do
-        python Fed_lora.py --lr ${lr} --path ./flearn/configs/LORA/${dataset}.json | tee ./res/lora/${dataset}_${lr}.txt
-    done
+    CUDA_VISIBLE_DEVICES=0 python Fed_lora.py --lr 7e-6 --path ./flearn/configs/LORA/${dataset}.json | tee ./res/lora/${dataset}_${lr}.txt &
+    CUDA_VISIBLE_DEVICES=1 python Fed_lpt.py --lr 7e-6 --path ./flearn/configs/LPT/${dataset}.json | tee ./res/lpt/${dataset}.txt &
+    CUDA_VISIBLE_DEVICES=2 python Fed_lpt.py --lr 7e-6 --path ./flearn/configs/ATTEMPT/${dataset}.json | tee ./res/attempt/${dataset}.txt &
+    CUDA_VISIBLE_DEVICES=3 python Fed_lpt.py --lr 7e-6 --path ./flearn/configs/PROMPT/${dataset}.json | tee ./res/prompt/${dataset}.txt
+    wait
 done
 
-# python Fed_lora.py --lr 1e-4 --path ./flearn/configs/LORA/rte.json
-
-# python Fed_lora_seq.py --lr 1e-4 --path ./flearn/configs/LORA/rte.json
-
-
-# lpt
-# for dataset in rte
-# do
-#     python Fed_lpt.py --lr 1e-4 --path ./flearn/configs/LPT/${dataset}.json | tee ./res/lpt/${dataset}.txt
-# done
-
-# # attempt
-# for dataset in rte
-#     python Fed_lpt.py --lr 1e-4 --path ./flearn/configs/ATTEMPT/${dataset}.json | tee ./res/attempt/${dataset}.txt
-# done
-
-# #prompt
-# for dataset in rte
-# do
-#     python Fed_lpt.py --lr 1e-4 --path ./flearn/configs/PROMPT/${dataset}.json | tee ./res/prompt/${dataset}.txt
-# done
-
-# # v2
-# for dataset in rte
-# do
-#     python Fed_v2.py --lr 1e-4 --path ./flearn/configs/V2/${dataset}.json | tee ./res/v2/${dataset}.txt
-# done
+for dataset in rte cola mrpc mpqa subj trec mr
+do
+    CUDA_VISIBLE_DEVICES=0 python Fed_v2.py --lr 7e-6 --path ./flearn/configs/V2/${dataset}.json | tee ./res/v2/${dataset}.txt
+done
