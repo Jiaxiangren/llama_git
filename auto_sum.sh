c27b8a66b2de4665726753fa3189b50fcb6fb6e5
jzr0065 <jzr0065@auburn.edu>
2024-02-11 05:42:58 -0600

    add

--- auto_sum.sh
+++ auto_sum.sh
@@ -15,19 +15,21 @@ done
 for dataset in rte cola mrpc mpqa subj trec mr
 do
     CUDA_VISIBLE_DEVICES=0 python Fed_v2.py --lr 7e-6 --path ./flearn/configs/V2/${dataset}.json | tee ./res/v2/${dataset}.txt &
-    CUDA_VISIBLE_DEVICES=1 python Fed_sLora.py --lr 7e-6 --select_method ours --select_layer_num 25 --path ./flearn/configs/ours/${dataset}.json | tee ./res/slora/${dataset}.txt &
-    CUDA_VISIBLE_DEVICES=2 python Fed_adaLora.py --lr 7e-6 --select_method ours --select_layer_num 25 --path ./flearn/configs/ours/${dataset}.json | tee ./res/adalora/${dataset}.txt &
-    CUDA_VISIBLE_DEVICES=3 python Fed_deltaLora.py --lr 7e-6 --select_method ours --select_layer_num 25 --path ./flearn/configs/ours/${dataset}.json | tee ./res/deltalora/${dataset}.txt
+    CUDA_VISIBLE_DEVICES=1 python Fed_sLora.py --lr 7e-6 --select_method ours --select_layer_num 32 --path ./flearn/configs/ours/${dataset}.json | tee ./res/slora/${dataset}.txt &
+    CUDA_VISIBLE_DEVICES=2 python Fed_adaLora.py --lr 7e-6 --select_method ours --select_layer_num 32 --path ./flearn/configs/ours/${dataset}.json | tee ./res/adalora/${dataset}.txt &
+    CUDA_VISIBLE_DEVICES=3 python Fed_deltaLora.py --lr 7e-6 --select_method ours --select_layer_num 32 --path ./flearn/configs/ours/${dataset}.json | tee ./res/deltalora/${dataset}.txt
     wait
 done
 
-#slora
-# CUDA_VISIBLE_DEVICES=0 python Fed_slora.py --lr 7e-6 --select_method ours --select_layer_num 24 --path ./flearn/configs/ours/rte.json
 
-#adalora
-# CUDA_VISIBLE_DEVICES=0 python Fed_adalora.py --lr 7e-6 --select_method ours --select_layer_num 24 --path ./flearn/configs/ours/rte.json
+for dataset in rte cola mrpc mpqa subj trec mr
+do
+    CUDA_VISIBLE_DEVICES=0 python Fed_llama_ours.py --lr 7e-6 --select_method increase --select_layer_num 25 --sort_type voc --path ./flearn/configs/ours/${dataset}.json | tee ./res/voc/${dataset}.txt &
+    CUDA_VISIBLE_DEVICES=1 python Fed_llama_ours.py --lr 7e-6 --select_method increase --select_layer_num 25 --sort_type seqreo --path ./flearn/configs/ours/${dataset}.json | tee ./res/seqreo/${dataset}.txt &
+    CUDA_VISIBLE_DEVICES=2 python Fed_llama_ours.py --lr 7e-6 --select_method increase --select_layer_num 25 --sort_type shortformer --path ./flearn/configs/ours/${dataset}.json | tee ./res/shortformer/${dataset}.txt
+    wait
+done
 
-#deltalora
-# CUDA_VISIBLE_DEVICES=0 python Fed_deltaLora.py --lr 7e-6 --select_method ours --select_layer_num 24 --path ./flearn/configs/ours/rte.json
+
 
 
